e9b0f0fe990a814506bd5c62a12d65c899d2fe81
ykapik307 <47310544+ykapik307@users.noreply.github.com>
2021-03-03 22:31:09 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,12 @@
+window.addEventListener('keydown', function(e){
+
+    //select corresponding audio
+    let audio = document.querySelector(`audio[data-key = "${e.keyCode}"]`);
+    let key=document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if (!audio){
+        return //stop the function from running all together
+      }
+      audio.currentTime = 0; // rewind to the start, here currentTime is an HTML property
+      audio.play();           // play is also an html function which is available with audio and video tags
+      key.classList.add('playing');
+    })
